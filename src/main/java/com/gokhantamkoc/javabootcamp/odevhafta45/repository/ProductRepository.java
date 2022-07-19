69e9b9b58b1b47da838c8d17e234bd8c8a186261
Eren <ernuygur@gmail.com>
2022-07-19 04:18:31 +0300

    third commit

--- src/main/java/com/gokhantamkoc/javabootcamp/odevhafta45/repository/ProductRepository.java
+++ src/main/java/com/gokhantamkoc/javabootcamp/odevhafta45/repository/ProductRepository.java
@@ -20,18 +20,86 @@ public class ProductRepository {
 
     public List<Product> getAll() {
         // BU METHODU 1. GOREV ICIN DOLDURUNUZ
+        final String SQL = "SELECT id, name, description FROM public.owner";
+        List<Product> products = new ArrayList<>();
+        try (PreparedStatement preparedStatement = databaseConnection.getConnection().prepareStatement(SQL)) {
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                String description = rs.getString("description");
+                products.add(
+                    new Owner(
+                        id,
+                        name,
+                        description
+                    )
+                );
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+        return products;
     }
 
     public Product get(long id) {
         // BU METHODU 1. GOREV ICIN DOLDURUNUZ
+        final String SQL = "SELECT * FROM public.product where id = ? limit 1;";
+        try (PreparedStatement preparedStatement = databaseConnection.getConnection().prepareStatement(SQL)) {
+            preparedStatement.setLong(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                return new Owner(
+                    rs.getLong("id"),
+                    rs.getString("name"),
+                    rs.getString("description")
+                );
+            } else {
+                return null;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex.getMessage());
+        }
     }
 
     public void save(Product product) throws RuntimeException {
         // BU METHODU 1. GOREV ICIN DOLDURUNUZ
+        final String SQL = "INSERT INTO public.product(id, name, description) values(?, ?, ?)";
+        try (PreparedStatement preparedStatement = databaseConnection.getConnection().prepareStatement(SQL)) {
+            preparedStatement.setLong(1, product.getId());
+            preparedStatement.setString(2, product.getName());
+            preparedStatement.setString(3, product.getDescription());
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows <= 0) {
+                throw new RuntimeException(String.format("Could not save product %s!", product.getName()));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex.getMessage());
+        }
+
     }
 
     public void update(Product product) throws RuntimeException {
         // BU METHODU 1. GOREV ICIN DOLDURUNUZ
+        Product foundProduct = this.get(product.getId());
+        if (foundProduct != null) {
+            final String SQL = "UPDATE public.product set name = ?, description = ? where id = ?";
+            try (PreparedStatement preparedStatement = databaseConnection.getConnection().prepareStatement(SQL)) {
+                preparedStatement.setString(1, product.getName());
+                preparedStatement.setString(2, product.getDescription());
+                preparedStatement.setLong(3, product.getId());
+                int affectedRows = preparedStatement.executeUpdate();
+                if (affectedRows <= 0) {
+                    throw new RuntimeException(String.format("Could not save product %s!", product.getName()));
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex.getMessage());
+            }
+        }
     }
 
     // BU METHODU SILMEYINIZ YOKSA TESTLER CALISMAZ
